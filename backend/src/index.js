6d3ad3ed466de7a51532cd4593ddb4b8cbbb1862
aryanaryan <103895665+aryankumar-dev@users.noreply.github.com>
2025-06-21 21:41:04 +0530

    save controller logic added

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 app.get("/", (req, res) => {
   res.send("Hello Guys welcome");
 });
+
 connectDB()
   .then(() => {
     app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
